61ed8eb4204825f98ca796dc74736da6a54e30f1
Tac Tacelosky <tacman@gmail.com>
2024-04-16 13:02:31 -0400

    tweak titles

--- assets/controllers/app_controller.js
+++ assets/controllers/app_controller.js
@@ -13,6 +13,7 @@ export default class extends Controller {
         'menu',
         'detail',
         'title',
+        'pageTitle',
         'tabbar',
         'twigTemplate',
         'savedCount','message','menu','navigator']
