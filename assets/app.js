acf854b05cf40b82d483a885414971a70a837e3e
Tac Tacelosky <tacman@gmail.com>
2024-12-03 13:17:18 -0500

    only load from assets, not cdn

--- assets/app.js
+++ assets/app.js
@@ -2,16 +2,19 @@ import './styles/app.css';
 import './components/loader.js';
 import './bootstrap.js'; // stimulus
 
-import 'https://cdn.jsdelivr.net/npm/onsenui@2.12.8/js/onsenui.min.js';
 // import 'onsenui'
 // import 'onsenui';
-window.ons = ons; // make global
 
 // without icons
 // import 'onsenui/css/onsenui-core.min.css'
 
+import 'onsenui/js/onsenui.min.js'
+// window.ons = ons; // make global
+
+
 import 'onsenui/css/onsenui-core.min.css'
 import 'onsenui/css/onsen-css-components.min.css'
+// import 'https://cdn.jsdelivr.net/npm/onsenui@2.12.8/js/onsenui.min.js';
 // we can probably be even more precise with https://www.jsdelivr.com/package/npm/onsenui?tab=files&path=css%2Fionicons%2Ffonts
 
 import 'onsenui/css/onsenui-fonts.min.css'
