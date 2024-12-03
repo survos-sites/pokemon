c2962ef47954aaf43c333b2fd0f62559e4e49350
Tac Tacelosky <tacman@gmail.com>
2024-12-03 14:07:57 -0500

    remove lazy load so it's always loaded

--- assets/controllers/ownswitch_controller.js
+++ assets/controllers/ownswitch_controller.js
@@ -4,7 +4,7 @@ import { Controller } from '@hotwired/stimulus';
 * The following line makes this controller "lazy": it won't be downloaded until needed
 * See https://github.com/symfony/stimulus-bridge#lazy-controllers
 */
-/* stimulusFetch: 'lazy' */
+// /* stimulusFetch: 'lazy' */
 export default class extends Controller {
     static values={
         owned: Boolean
