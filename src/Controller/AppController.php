3cdb3678d822681b481ff772e978c8bc776ce4b3
Tac Tacelosky <tacman@gmail.com>
2024-12-17 19:07:15 -0500

    use onsMeta

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,29 +2,81 @@
 
 namespace App\Controller;
 
+use Knp\Menu\FactoryInterface;
+use Survos\MobileBundle\Event\KnpMenuEvent;
 use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AppController extends AbstractController
 {
+
+    public function __construct(
+        private EventDispatcherInterface $eventDispatcher,
+        protected FactoryInterface $factory,
+    )
+    {
+    }
+
     /**
      * @return Response
      *
      * this is the main controller, really it's a SPA at this point
      */
-    #[Route('/', name: 'app_index')]
-    public function index(): Response
+//    #[Route('/', name: 'app_index')]
+//    public function index(): Response
+//    {
+//        // render the "pages"
+//        $templates = [];
+//        foreach (['about','gallery','home','pokemon','saved','login','share','detail','loading'] as $route) {
+//            $templates[$route] = $this->renderView("app/$route.html.twig");
+//        }
+//        return $this->render('app/index.html.twig', [
+//            'templates' => $templates,
+//        ]);
+//    }
+
+    #[Route('/', name: 'app_index', options: ['expose' => true], methods: ['GET'])]
+    public function mobile(Request $request): Response
     {
-        // render the "pages"
-        $templates = [];
-        foreach (['about','gallery','home','pokemon','saved','login','share','detail','loading'] as $route) {
-            $templates[$route] = $this->renderView("app/$route.html.twig");
+
+        $menu = $this->factory->createItem($options['name'] ?? KnpMenuEvent::class);
+        foreach ([KnpMenuEvent::MOBILE_TAB_MENU  => 'tab',
+
+                     KnpMenuEvent::MOBILE_PAGE_MENU => 'page',
+                     KnpMenuEvent::MOBILE_UNLINKED_MENU => 'unlinked',
+                     ] as $eventName=>$type) {
+            $options = [];
+            $options = (new OptionsResolver())
+                ->setDefaults([
+
+                ])
+                ->resolve($options);
+            $this->eventDispatcher->dispatch(new KnpMenuEvent($menu, $this->factory, $options), $eventName);
+            foreach ($menu->getChildren() as $route=>$child) {
+                try {
+                $template = "app/$route.html.twig";
+                $params = [
+                    'type' => $type,
+                    'route' => $route,
+                    'template' => $template,
+                    'debug' => $request->get('debug', false),
+                ];
+//                    $templates[$route]  = $this->twig->render($template, $params);
+                    $templates[$route] = $this->renderView($template, $params);
+                } catch (\Exception $e) {
+                    dd($route, $template, $e->getMessage(), $e);
+                }
+            }
         }
-        return $this->render('app/index.html.twig', [
+
+        return $this->render('start.html.twig', [
             'templates' => $templates,
+            'playNow' => $request->get('playNow', true),
         ]);
     }
 
